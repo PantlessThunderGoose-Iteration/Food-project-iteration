50c1a843f786e6f08eaf46f923b59a3003c24e36
shelbycarlin <shelbycarlin13@gmail.com>
2022-06-29 20:04:32 -0700

    styling and elliott fif some handler stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './styling/index.css';
+import './styling/styles.scss';
 import App from './containers/App';
 
 
